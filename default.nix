178b5356abf599c68cda45016ae0c54f7cfeaa65
notgne2 <gen2@gen2.space>
2020-03-04 22:03:16 -0700

    allow user-supplied nixpkgs channel

--- default.nix
+++ default.nix
@@ -1,21 +1,9 @@
+{ nixpkgs ? <nixpkgs>, pkgs ? import nixpkgs { }, ... }:
+
 {
-  ...
-}:
-
-rec {
-  nixpkgs = <nixpkgs>;
-  # it's a good idea not to use any of these, the top one breaks things iirc
-  # pkgs = import nixpkgs { overlays = [ (self: super: { glibcLocales = super.glibcLocales.override { allLocales = false; }; }) ]; };
-  # pkgs = (import nixpkgs {}).pkgsMusl;
-  pkgs = import nixpkgs {};
-
   # Generate a Docker image from a system and some other props
-  mkDocker = pkgs.callPackage ./docker.nix {
-    inherit nixpkgs pkgs;
-  };
+  mkDocker = pkgs.callPackage ./docker.nix { inherit nixpkgs pkgs; };
 
   # Make a system from a NixOS-like config (think configuration.nix)
-  mkSystem = pkgs.callPackage ./system.nix {
-    inherit nixpkgs pkgs;
-  };
+  mkSystem = pkgs.callPackage ./system.nix { inherit nixpkgs pkgs; };
 }
