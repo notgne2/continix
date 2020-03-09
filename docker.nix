cfc229d05dc123f388a36cd96b9d3e3a760a87d2
notgne2 <gen2@gen2.space>
2020-03-08 21:13:18 -0700

    use modded dockertools to include multiple pkgs per layer

--- docker.nix
+++ docker.nix
@@ -1,40 +1,28 @@
-{
-  nixpkgs,
-  pkgs,
-  lib,
+{ nixpkgs, pkgs, lib, ... }:
 
-  ...
-}:
+let
+  repeat = n: a: accum:
+    if n == 0 then accum else repeat (n - 1) a (accum ++ [ a ]);
+  r = n: a: repeat n a [ ];
+in ({ cont, name ? cont.name, tag, cmd ? [ ], workDir ? "/data"
+  , enableLocale ? false, extraDockerConfig ? { }, env ? [ ], maxLayers ? 120
+  , perLayer ? 3, hints ? r (maxLayers - 1) perLayer }:
 
-{
-  cont,
-  name ? cont.name,
-  tag,
-  cmd ? [],
-  workDir ? "/data",
-  enableLocale ? false,
-  extraDockerConfig ? {},
-  env ? [],
-  maxLayers ? 120,
-}:
+  let dockerTools = pkgs.callPackage (./dockertools/default.nix) { };
+  in dockerTools.buildLayeredImage {
+    inherit maxLayers name tag hints;
 
-pkgs.dockerTools.buildLayeredImage {
-  inherit maxLayers name tag;
+    contents = pkgs.symlinkJoin {
+      name = "${name}-contents";
+      paths = [ cont.sys.build.etc cont.sys.path ] ++ cont.contents;
+    };
 
-  contents = pkgs.symlinkJoin {
-    name = "${name}-contents";
-    paths = [
-      cont.sys.build.etc
-      cont.sys.path
-    ] ++ cont.contents;
-  };
+    config = {
+      Entrypoint = cont.entrypoint;
 
-  config = {
-    Entrypoint = cont.entrypoint;
+      Cmd = cmd;
+      WorkingDir = workDir;
 
-    Cmd = cmd;
-    WorkingDir = workDir;
-
-    Env = cont.env;
-  } // extraDockerConfig;
-}
+      Env = cont.env;
+    } // extraDockerConfig;
+  })
